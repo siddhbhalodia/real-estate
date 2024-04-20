2adc4220a0687f703108aa78a3fc5e757b204ae5
vatsal thummar <tvatsal04@gmail.com>
2024-04-20 23:46:39 +0530

    Created backend server and tested  api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+export const register = (req, res) => {
+    //db operations
+    const {username,email,password}=req.body;
+    //HASH THE PASSWORD
+    //CREATE A NEW USER AND SAVE TO DB
+}
+export const login = (req, res) => {
+    //db operations
+}
+export const logout = (req, res) => {
+    //db operations
+}
---
+++ api/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+
+const router=express.Router();
+
+// router.get('/test',(req,res)=>{
+//     console.log("it works")
+// })
+// router.post('/test',(req,res)=>{
+//     console.log("it works")
+// })
+// router.put('/test',(req,res)=>{
+//     console.log("it works")
+// })
+// router.delete('/test',(req,res)=>{
+//     console.log("it works")
+// })
+export default router
