3bfe508585b1280e711f6cc86861dfab1f1c3c74
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-04-21 18:14:42 +0530

    added jsonwebtoken and created api for login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -29,8 +30,45 @@ export const register = async (req, res) => {
         res.status(500).json({message: "Failed to create the user!"})
     }
 }
-export const login = (req, res) => {
-    //db operations
+export const login = async (req, res) => {
+    const {username , password} = req.body;
+
+    try{
+
+        // Check if user exist
+
+        const user = await prisma.user.findUnique({
+            where:{username}
+        })
+
+        if(!user) res.status(401).json({message: "Invalid credentials"})
+        
+        // check if password is correct
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid) res.status(401).json({message: "Invalid credentials"})
+        
+        // generate cookie token and send to user   
+
+        const age= 1000*60*60*24*7
+
+        const token = jwt.sign({
+            id: user.id
+        },process.env.JWT_SECRET_KEY,
+        {expiresIn: age}
+        );
+
+        res.cookie("token",token,{
+            httpOnly: true,
+            // secure: true,
+            maxAge: age,
+        }).status(200).json({message: "Login Successful!!"})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to login!"})
+    }
 }
 export const logout = (req, res) => {
     //db operations
