5f620b4a1eae8736d930a48bd89e5c531ac7e198
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-04-21 15:43:15 +0530

    Prisma initialized and User Prisma created

--- api/app.js
+++ api/app.js
@@ -7,6 +7,6 @@ app.use(express.json())
 
 app.use("/api/posts",postRoute)
 app.use("/api/auth",authRoute)
-app.listen(8000,()=>{
+app.listen(8800,()=>{
     console.log("server is running")
 })
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,33 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
     //db operations
     const {username,email,password}=req.body;
-    //HASH THE PASSWORD
-    //CREATE A NEW USER AND SAVE TO DB
+
+    try{
+
+        //HASH THE PASSWORD
+            const hashedPassword = await bcrypt.hash(password,10);
+            // console.log(hashedPassword)
+            
+            //CREATE A NEW USER AND SAVE TO DB
+        const newUser = await prisma.user.create({
+            data:{
+                username,
+                email,
+                password: hashedPassword,
+            }
+        })
+        
+        // console.log(newUser)
+        
+        res.status(201).json({message: "User created successfully"})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to create the user!"})
+    }
 }
 export const login = (req, res) => {
     //db operations
