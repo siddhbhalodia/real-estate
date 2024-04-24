10428919bd1af9929009c62dc90703874736b19a
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-04-24 17:19:26 +0530

    added post Routes and controllers

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,46 @@
+import prisma from "../lib/prisma.js"
+
+export const getPosts = async (req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
+
+export const getPost = async (req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
+
+export const addPost = async (req,res) => {
+    try{
+        
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
+
+export const updatePost = async (req,res) => {
+    try{
+        
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
+
+export const deletePost = async (req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to get posts"})
+    }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,18 +1,13 @@
 import express from "express"
-
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router=express.Router();
 
-// router.get('/test',(req,res)=>{
-//     console.log("it works")
-// })
-// router.post('/test',(req,res)=>{
-//     console.log("it works")
-// })
-// router.put('/test',(req,res)=>{
-//     console.log("it works")
-// })
-// router.delete('/test',(req,res)=>{
-//     console.log("it works")
-// })
+router.get("/",getPosts)
+router.get("/:id",getPost)
+router.post("/",verifyToken,addPost)
+router.put("/:id",verifyToken,updatePost)
+router.delete("/:id",verifyToken,deletePost)
+
 export default router
